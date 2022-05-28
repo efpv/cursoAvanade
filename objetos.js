4f3c7aaa00de1519048840ef84a58ad66da22649
efpv <erick.vieira@outlook.com.br>
2022-05-28 14:50:36 -0300

    Aula de JavaScript atividades

--- objetos.js
+++ objetos.js
@@ -1,16 +1,19 @@
 //1 - Faça uma função que retorne um objeto que represente você, com os seguintes atributos: 
-//altura: ponto flutuante
-//corCabelo: string
-//cursos: array
+
 //escreverNome: função que escreve seu nome
 //filhos: array de objetos, o objeto filho precisa ter nome e idade
-//habilitado: boolean
-//idade: inteiro
-//mae: objeto com nome e idade
 //nome: string
+//idade: inteiro
+//altura: ponto flutuante
+//habilitado: boolean
+//corCabelo: string
+//cursos: array
+//mae: objeto com nome e idade
 
-{nome: "Erick", idade: 21, altura: 1.70, habilitado: true}
+var pessoa = {nome: "Erick", idade: 37, altura: 1.70, habilitado: true, cabelo: "castanho", cursos: ["LPIC1","AZURE ESSENTIALS","ORACLE OCI"], filhos: [{nome: "Isaac", idade: 15},{nome: "Lucas", idade: 6}], mae: {nome: "Roseli", idade: 72, altura: 1.54}}
 
+function exibirPessoa (){
+    return pessoa;}
 //2 - faça uma função que utilize a função anterior e filtre pelos filhos com mais de 1 ano de idade.
 
 //3 - faça uma função que utilize a função 1 e obtenha somente o nome dos filhos com mais de 1 ano de idade.
